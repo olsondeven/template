2fdcb310ae8a010dce94a73127f3033f99b6893a
olsondeven <olsondeven@gmail.com>
2017-04-10 16:51:37 -0600

    game obj setup in main service

--- public/app/services/mainService.js
+++ public/app/services/mainService.js
@@ -1,3 +1,58 @@
 angular.module('app').service('mainService',function($http){
- 
+  //info for singles games
+ let singleGame = {
+   date: null,//date stamp
+   selectPoint: null,//
+   selectMatch: null,
+   selectType: null,
+   player1: {
+     name: null,
+     gameScore: null,
+     matchScore: null,
+     foul: null,
+     let: null,
+     hasAcct: false
+   },
+   player2: {
+     name: null,
+     gameScore: null,
+     matchScore: null,
+     foul: null,
+     let: null,
+     hasAcct: false
+   },
+   matchWinner: null,
+   matchLoser: null,
+   curSer: null,
+   startSer: null,
+   save: false,
+   login: {
+     currentUser: null
+   },
+   switchSer: null,
+   tiebreaker: null,
+   team1: {
+     teamName: null,
+     foul: null,
+     let: null,
+     gameScore: null,
+     matchScore: null,
+     hasAcct: false,
+     selectedSErvice: null,
+     mate1: null,
+     mate2: null
+   },
+   team2: {
+     teamName: null,
+     foul: null,
+     let: null,
+     gameScore: null,
+     matchScore: null,
+     hasAcct: false,
+     selectedSErvice: null,
+     mate1: null,
+     mate2: null
+   }
+ }
+ //get/set/update/delete from controllers and views
 });//closing
