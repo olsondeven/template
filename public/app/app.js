b09167d65a1114719a132744ccd636f939d31337
olsondeven <olsondeven@gmail.com>
2017-04-10 18:28:29 -0600

    home style complete

--- public/app/app.js
+++ public/app/app.js
@@ -8,6 +8,26 @@ angular.module('app', ['ui.router'])
     url: '/',
     templateUrl: './app/routes/home/homeTemp.html',
     controller: 'homeCtrl'
+  })
+  .state('flip', {
+    url: '/flip',
+    templateUrl: './app/routes/flip/flipTemp.html',
+    controller: 'flipCtrl'
+  })
+  .state('game', {
+    url: '/game',
+    templateUrl: './app/routes/game/gameTemp.html',
+    controller: 'gameCtrl'
+  })
+  .state('match', {
+    url: '/match',
+    templateUrl: './app/routes/match/matchTemp.html',
+    controller: 'matchCtrl'
+  })
+  .state('point', {
+    url: '/point',
+    templateUrl: './app/routes/point/pointTemp.html',
+    controller: 'pointCtrl'
   });
 
   $urlRouterProvider.otherwise('/');
