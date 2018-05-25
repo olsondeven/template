78233f7fb5d2648c6fcb7ca7c939eb569b522788
olsondeven <olsondeven@gmail.com>
2018-05-25 17:47:20 -0600

    update

--- controller/mainCtrl.js
+++ controller/mainCtrl.js
@@ -6,22 +6,15 @@ function getIpConfig(req,res,nex){
   if(req.params.ip){
     curIp = req.params.ip;
     cmd.get(
-      'ping -a '+curIp,
+      // 'ping -a '+curIp,
+      ["ping -a "+curIp],
       function(err, data, stderr){
-        console.log('the current working dir is : ',data)
+        console.log('the current working dir is : ',data);
+        console.log('CMD first get: ',cmd);
         if(data){
           return res.status(200).send(data);
         }
-      }
-    );
-  }else {
-    cmd.get(
-      'ipconfig',
-      function(err, data, stderr){
-        //console.log('the current working dir is : ',data)
-        return res.status(200).send(data);
-      }
-    );
+      });
   }
 }
 module.exports = {getIpConfig};
